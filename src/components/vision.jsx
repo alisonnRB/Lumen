6c1b80ea19a7d7cc88641f3912ca4d47343d4a77
alisonnRB <alisonrbjs@gmail.com>
2024-07-16 19:49:29 -0300

    vision in crating

---
+++ src/components/vision.jsx
@@ -0,0 +1,17 @@
+import capa from '../assets/capaPage.jpg';
+import contate from '../assets/contatePage.png';
+
+export default function Vision(){
+    return(
+        <section className='bg-page w-[100dvw] h-[1000px] vision-content'>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+
+            <div>
+
+            </div>
+        </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Initial from "@/components/initial";
 import About from "@/components/about";
+import Vision from "@/components/vision";
 
 export default function Home() {
   return (
     <main>
       <Initial />
       <About />
+      <Vision />
     </main>
   );
 }
