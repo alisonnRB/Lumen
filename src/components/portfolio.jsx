40ab5fb770e8814ca06bf6b23ca474dcad35a7e7
alisonnRB <alisonrbjs@gmail.com>
2024-07-19 13:15:26 -0300

    att

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import book from '../assets/books.jpg';
 import product from '../assets/products.jpg';
+import thumb from '../assets/model_thumb.png';
 
 import Link from "next/link";
 
@@ -12,10 +13,21 @@ export default function Portfolio() {
 
             <div className="grid grid-cols-2 grid-rows-2">
 
-                <div className="relative col-span-1 row-span-2 z-10 flex items-end">
-                    <video autoPlay loop muted className="w-full h-full object-cover" alt="Video de uma model, em um ensaio fotografico da lumen.">
+                <div className="relative col-span-1 row-span-2 flex items-end">
+                    <video autoPlay loop muted className="w-full h-full object-cover relative z-20" alt="Video de uma model, em um ensaio fotografico da lumen.">
                         <source src="/video/model.mp4" type="video/mp4" />
                     </video>
+
+                    <div className="w-full h-full absolute z-10">
+                        <Image
+                            src={thumb}
+                            alt="Fotografia feita pela lumen em um ensaio fotografico"
+                            style={{ objectFit: 'contain' }}
+                            quality={100}
+                        />
+                    </div>
+
+
 
                     <h6 className="text-page font-bold absolute z-20 ml-4 titlePort">MODEL</h6>
                 </div>
@@ -23,7 +35,7 @@ export default function Portfolio() {
                 <div className="relative col-span-1 z-10">
                     <Image
                         src={book}
-                        alt="Fotografia feita pelas lumen de dois irmão modelos para almbum de musica"
+                        alt="Fotografia feita pela lumen de dois irmão modelos para almbum de musica"
                         fill={true}
                         style={{ objectFit: 'cover' }}
                         quality={100}
